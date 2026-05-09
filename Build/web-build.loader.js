344523279ddc6a5cc7f85c28795ae35286ac619c
Vinicius Lopes <viniloppes2000@gmail.com>
2026-05-08 23:16:12 -0300

    inserindo sound effects e terrain. forcando orientacao landscape

--- Build/web-build.loader.js
+++ Build/web-build.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 61744969,
+    wasmFileSize: 61905756,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
